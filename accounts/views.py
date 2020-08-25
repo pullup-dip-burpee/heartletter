23d74723ee1a4778e4ee1a9ab8b6ca707b377c42
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2020-08-25 12:02:36 +0000

    Add signup in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'home.html')
-# Create your views here.
+
+def sign_up(request):
+    if request.method == "POST":
+        if request.POST["password"] == request.POST["password2"]:
+            user = User.objects.create_user(
+                    username = request.POST["username"],
+                    password = request.POST["password"],
+            )
+            auth.login(request, user)
+            return redirect('main')
+        return render(request, 'sign_up.html')
