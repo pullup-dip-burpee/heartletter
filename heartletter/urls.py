922ccede8474196fc55d777a69661ad60aaac385
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2020-08-25 13:38:52 +0000

    Modify account app

--- heartletter/urls.py
+++ heartletter/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import account.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', accounts.views.home, name='home'),
+    path('', account.views.home, name='home'),
 ]
